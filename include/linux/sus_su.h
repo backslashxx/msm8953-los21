02694f0b5d1fa724b6f52d5ade90ef94d3debbcb
backslashxx <118538522+backslashxx@users.noreply.github.com>
2024-11-10 21:52:21 +0800

    susfs: adapt to this tree

--- include/linux/sus_su.h
+++ include/linux/sus_su.h
@@ -1,8 +1,8 @@
 #ifndef __KSU_H_SUS_SU
 #define __KSU_H_SUS_SU
 
-#include "../../drivers/kernelsu/core_hook.h"
-#include "../../drivers/kernelsu/sucompat.h"
+#include "../../KernelSU/kernel/core_hook.h"
+#include "../../KernelSU/kernel/sucompat.h"
 
 int sus_su_fifo_init(int *maj_dev_num, char *drv_path);
 int sus_su_fifo_exit(int *maj_dev_num, char *drv_path);
